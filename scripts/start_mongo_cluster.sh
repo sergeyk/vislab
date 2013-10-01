1060e6f4cddbfdba618f979f3309b370fdcef6da
Sergey Karayev <sergeykarayev@gmail.com>
2013-09-30 20:56:04 -0700

    getting rid of data_shared/ in mongo & redid cluster scripts

--- scripts/start_mongo_cluster.sh
+++ scripts/start_mongo_cluster.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-mkdir -p data_shared/db/
-srun -p vision --nodelist=flapjack --mem=72000 --time=0 --output=data_shared/db/job.log ./scripts/start_mongo.sh &
+mkdir -p data/shared/db/
+srun -p vision --nodelist=flapjack --mem=72000 --time=0 --output=data/shared/db/job.log ./scripts/start_mongo.sh &
--- scripts/start_redis_cluster.sh
+++ scripts/start_redis_cluster.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-mkdir -p data_shared/redis
-srun -p vision --nodelist=flapjack --mem=8000 --time=0 --output=data_shared/redis/job.log --chdir=./data/redis redis-server &
+mkdir -p data/shared/redis
+srun -p vision --nodelist=flapjack --mem=8000 --time=0 --output=data/shared/redis/job.log --chdir=./data/redis redis-server &
