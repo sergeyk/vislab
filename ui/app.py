4dce024a3550cff4665f065f8f302054a3d424ca
Sergey Karayev <sergeykarayev@gmail.com>
2014-02-28 11:24:54 -0800

    new UI updated with filtering by query and pagination

--- ui/app.py
+++ ui/app.py
@@ -8,8 +8,7 @@ TODO
 DataFrames here.
 - deal with getting many of a single user's pins at once: maybe only
 display at most N pins per user.
-- add drop-down select menu to select which style to view.
-- add pagination
+- display info about image under it
 """
 import os
 import flask
@@ -19,21 +18,43 @@ app = flask.Flask(__name__)
 
 pins_df = pd.read_hdf(os.path.expanduser(
     '~/work/vislab/data/shared/pins_df_feb27.h5'), 'df')
+query_names = [
+    _[6:] for _ in pins_df.columns.tolist()
+    if _.startswith('query_')
+]
 
 
 @app.route('/')
-def data():
-    df = pins_df.iloc[:50]
-    images = [_[1].to_dict() for _ in df.iterrows()]
-    return flask.render_template('data.html', images=images)
+def index():
+     return flask.redirect(flask.url_for('data', style_name='all', page=1))
 
 
-@app.route('/<style_name>')
-def data_for_style(style_name):
-    df = pins_df[pins_df['query_{}'.format(style_name)]].iloc[:50]
+@app.route('/data/<style_name>/<int:page>')
+def data(style_name, page):
+    results_per_page = 60
+
+    # Filter on style.
+    df = pins_df
+    if style_name != 'all':
+        df = pins_df[pins_df['query_{}'.format(style_name)]]
+
+    # Paginate
+    num_pages = df.shape[0] / results_per_page
+    start = page * results_per_page
+    df = df.iloc[start:min(df.shape[0], start + results_per_page)]
+
+    # Set filter options
+    select_options = [
+        ('query', ['all'] + query_names, style_name),
+        ('page', range(1, num_pages), page)
+    ]
+
+    # Fetch images and render.
     images = [_[1].to_dict() for _ in df.iterrows()]
-    return flask.render_template('data.html', images=images)
+    return flask.render_template(
+        'data.html', images=images, select_options=select_options
+    )
 
 
 if __name__ == '__main__':
-    app.run(debug=False, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
