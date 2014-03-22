24fb897d48d64f2b02074f048aa75d3d8fa33cfa
Sergey Karayev <sergeykarayev@gmail.com>
2014-03-21 17:17:35 -0700

    config.json.example and push_to_cluster bugfix

--- scripts/push_to_cluster.sh
+++ scripts/push_to_cluster.sh
@@ -1,11 +1,13 @@
 #!/bin/zsh
 
 # rsync the repo to cluster
-
 # code
 rsync -ravzP --delete --prune-empty-dirs \
     --exclude=".DS_Store" --exclude='*.pyc' --exclude='data' --exclude='vislab/tests/_temp' \
-    . sergeyk@flapjack.icsi.berkeley.edu:/u/sergeyk/work/vislab
+    . sergeyk@flapjack1.icsi.berkeley.edu:/u/sergeyk/work/vislab
+rsync -ravzP --delete --prune-empty-dirs \
+    --exclude=".DS_Store" --exclude='*.pyc' --exclude='data' --exclude='vislab/tests/_temp' \
+    . sergeyk@flapjack2.icsi.berkeley.edu:/u/sergeyk/work/vislab
 
 # shared data
 rsync -ravzP --delete data/shared/ sergeyk@flapjack1.icsi.berkeley.edu:/u/sergeyk/work/vislab/data/shared
