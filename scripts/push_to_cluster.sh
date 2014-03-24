fcf515f2080f5e4dd8dde19238d7fd5a689d6819
Sergey Karayev <sergeykarayev@gmail.com>
2014-03-24 11:45:51 -0700

    Misc cluster fixes

--- scripts/push_to_cluster.sh
+++ scripts/push_to_cluster.sh
@@ -7,12 +7,8 @@ rsync -ravzP --delete --prune-empty-dirs \
     . sergeyk@flapjack1.icsi.berkeley.edu:/u/sergeyk/work/vislab-git
 rsync -ravzP --delete --prune-empty-dirs \
     --exclude "vislab/config.json" --exclude=".DS_Store" --exclude='*.pyc' --exclude='data' --exclude='vislab/tests/_temp' \
-    . sergeyk@flapjack2.icsi.berkeley.edu:/u/sergeyk/work/vislab-git
-rsync -ravzP --delete --prune-empty-dirs \
-    --exclude "vislab/config.json" --exclude=".DS_Store" --exclude='*.pyc' --exclude='data' --exclude='vislab/tests/_temp' \
     . sergeyk-pc1.vicsi:work/vislab-git
 
 # shared data
 rsync -ravzP --delete data/shared/ sergeyk@flapjack1.icsi.berkeley.edu:/u/sergeyk/work/vislab-git/data/shared
-rsync -ravzP --delete data/shared/ sergeyk@flapjack2.icsi.berkeley.edu:/u/sergeyk/work/vislab-git/data/shared
 rsync -ravzP --delete data/shared/ sergeyk-pc1.vicsi:work/vislab-git/data/shared
