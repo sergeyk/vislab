2e37de46dbd397e1952a3371c67d9807ed8e357e
Sergey Karayev <sergeykarayev@gmail.com>
2014-04-29 16:48:40 -0700

    Updated UI to show prediction results

--- ui/app.py
+++ ui/app.py
@@ -4,28 +4,51 @@ The older code for the same tasks is in vislab/app.py
 We start with the Pinterest data here.
 
 TODO
-- port the result-displaying capability from old vislab/app.py
+- switch from using dataframes for results to mongo
+- make image page that displays all the true labels and pred values for
+    a given image
 """
 import flask
 import tornado.wsgi
 import tornado.httpserver
-import numpy
+import numpy as np
 import time
+import pandas as pd
+from collections import defaultdict
 import vislab.datasets
+import vislab._results
 
 mongo_client = vislab.util.get_mongodb_client()
 app = flask.Flask(__name__)
 style_names = vislab.datasets.flickr.underscored_style_names
 
 
-@app.route('/')
-def index():
+def load_pred_results(all_preds, results_dirname, experiment_name):
     """
-    Redirect to the data page for Flickr:Pastel.
+    Load prediction results into accessible form.
+    Also, make positive predictions positive and negative negative, to
+    simplify future sorting by confidence.
     """
-    return flask.redirect(flask.url_for(
-        'data', dataset_name='flickr', style_name='style_Pastel', page=1)
-    )
+    threshold_df = pd.read_hdf(
+        '{}/{}_thresholds.h5'.format(results_dirname, experiment_name), 'df')
+
+    _, preds_panel = vislab._results.load_pred_results(
+        experiment_name, results_dirname,
+        multiclass=True, force=False)
+
+    # TODO: load into mongo collections instead
+    for setting in preds_panel.minor_axis:
+        df_ = preds_panel.minor_xs(setting)
+        for style_name in style_names:
+            df_['pred_' + style_name] -= threshold_df.loc[style_name, setting]
+            df_['abs_pred_' + style_name] = np.abs(df_['pred_' + style_name])
+        all_preds[experiment_name][setting] = df_
+
+
+results_dirname = vislab.config['paths']['shared_data'] + '/results_mar23'
+all_preds = defaultdict(dict)  # TODO: use a mongo database instead
+load_pred_results(all_preds, results_dirname, 'flickr_mar23')
+flickr_df = vislab.datasets.flickr.get_df()  # TODO: get rid of this
 
 
 def get_collection(dataset_name):
@@ -52,11 +75,120 @@ def get_collection(dataset_name):
 
             d = df.iloc[i].to_dict()
             for k, v in d.iteritems():
-                if type(d[k]) is numpy.bool_:
+                if type(d[k]) is np.bool_:
                     d[k] = bool(d[k])
             collection.insert(d)
 
     return collection
+
+
+@app.route('/')
+def index():
+    """
+    Redirect to the data page for Flickr:Pastel.
+    """
+    return flask.redirect(flask.url_for(
+        'data', dataset_name='flickr', style_name='style_Pastel', page=1
+    ))
+
+
+@app.route('/results')
+def results_default():
+    return flask.redirect(flask.url_for(
+        'results', experiment='flickr_mar23', setting='caffe_fc6 None vw',
+        style='style_Vintage', split='test',
+        gt_label='all', pred_label='positive', confidence='decreasing', page=1
+    ))
+
+
+@app.route('/results/<experiment>/<setting>/<style>/<split>/<gt_label>/<pred_label>/<confidence>/<int:page>')
+def results(experiment, setting, style, split, gt_label, pred_label,
+            confidence, page):
+    """
+    experiment:
+        name of collection that held the results
+        e.g. 'flickr_mar23'
+    setting:
+        features and special techniques used in classification
+        e.g. 'caffe_fc6 None vw'
+    style:
+        name of predicted attribute
+        e.g. 'style_Vintage'
+    split:
+        ['all', 'train', 'val', 'test']
+    gt_label:
+        ground truth value of style_name
+        ['all', 'positive', 'negative']
+    pred_label:
+        predicted value of style_name
+        ['all', 'positive', 'negative']
+    confidence:
+        ['increasing', 'decreasing']
+    page:
+        index of paginated results
+    """
+    # TODO: use mongo throughout
+
+    df = all_preds[experiment][setting]
+
+    if split != 'all':
+        df = df[df['split'] == split]
+
+    if gt_label == 'positive':
+        df = df[df[style]]
+    elif gt_label == 'negative':
+        df = df[df[style] == False]
+
+    if pred_label == 'positive':
+        df = df[df['pred_' + style] > 0]
+    elif pred_label == 'negative':
+        df = df[df['pred_' + style] <= 0]
+
+    if confidence == 'increasing':
+        df.sort('abs_pred_' + style, ascending=True, inplace=True)
+    else:
+        df.sort('abs_pred_' + style, ascending=False, inplace=True)
+
+    num_results = df.shape[0]
+    results_per_page = 7 * 20
+    num_pages = num_results / results_per_page
+
+    start_ind = (page - 1) * results_per_page
+    end_ind = min(num_results, start_ind + results_per_page)
+
+    if num_results > 0:
+        df = df.iloc[start_ind:end_ind]
+        df['page_url'] = flickr_df['page_url']
+        df['image_url'] = flickr_df['image_url']
+
+        caption = lambda row: 'conf: {:.2f} | gt: {}'.format(
+            row['pred_' + style],
+            '+' if row[style] else '-'
+        )
+        df['caption'] = df.apply(caption, axis=1)
+        results = [_.to_dict() for i, _ in df.iterrows()]
+    else:
+        results = []
+
+    # Set filter options. Order matters.
+    select_options = [
+        ('experiment', ['flickr_mar23'], experiment),
+        ('setting', all_preds[experiment].keys(), setting),
+        ('style', ['all'] + style_names, style),
+        ('split', ['all', 'train', 'val', 'test'], split),
+        ('gt_label', ['all', 'positive', 'negative'], gt_label),
+        ('pred_label', ['all', 'positive', 'negative'], pred_label),
+        ('confidence', ['increasing', 'decreasing'], confidence),
+        ('page', range(1, num_pages), page)
+    ]
+
+    return flask.render_template(
+        'data.html', images=results, select_options=select_options,
+        num_results=num_results,
+        start_results=results_per_page * (page - 1),
+        end_results=results_per_page * page,
+        page_type='results'
+    )
 
 
 @app.route('/data/<dataset_name>/<style_name>/<int:page>')
@@ -85,14 +217,12 @@ def data(dataset_name, style_name, page):
         ('page', range(1, num_pages), page),
     ]
 
-    # Fetch images and render.
-    images = results
-
     return flask.render_template(
-        'data.html', images=images, select_options=select_options,
+        'data.html', images=results, select_options=select_options,
         num_results=num_results,
         start_results=results_per_page * (page - 1),
-        end_results=results_per_page * page
+        end_results=results_per_page * page,
+        page_type='data'
     )
 
 
