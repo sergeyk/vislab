25673883e677536019a620a5619d1d6bf47e7e62
Sergey Karayev <sergeykarayev@gmail.com>
2014-04-29 16:48:16 -0700

    found thresholds for best flickr style accuracies

--- scripts/push_to_sergeyk_pc1.sh
+++ scripts/push_to_sergeyk_pc1.sh
@@ -1,12 +1,10 @@
 #!/bin/zsh
 
 # rsync the repo to cluster
-echo "Run on cluster"
-
 # code
 rsync -ravzP --delete --prune-empty-dirs \
     --exclude=".DS_Store" --exclude='*.pyc' --exclude='data' --exclude='vislab/tests/_temp' \
-    . sergeyk-pc1:~/work/vislab
+    . sergeyk-pc1.vicsi:~/work/vislab-git
 
 # shared data
-rsync -ravzP --delete data/shared/ sergeyk-pc1:~/work/vislab/data/shared
+rsync -ravzP --delete data/shared/ sergeyk-pc1.vicsi:~/work/vislab-git/data/shared
