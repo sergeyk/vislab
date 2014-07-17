12f55becb6b531afdd38b0884d887e35cedbc9e9
Sergey Karayev <sergeykarayev@gmail.com>
2014-07-17 11:54:34 -0700

    FIX clean up push/pull scripts

--- scripts/pull_from_cluster.sh
+++ scripts/pull_from_cluster.sh
@@ -1,8 +1,11 @@
 #!/bin/zsh
+echo "usage: pull_from_cluster <remote>"
+REMOTE=$1
 
-rsync -rtazP sergeyk@flapjack1.icsi.berkeley.edu:/u/sergeyk/work/vislab-git/data/shared/rqworkers ./data/shared/
-rsync -rtazP sergeyk@flapjack1.icsi.berkeley.edu:/u/sergeyk/work/vislab-git/data/shared/predict ./data_shared/
-rsync -rtazP sergeyk@flapjack1.icsi.berkeley.edu:/u/sergeyk/work/vislab-git/data/shared/db ./data/shared/
-rsync -rtazP sergeyk@flapjack1.icsi.berkeley.edu:/u/sergeyk/work/vislab-git/data/shared/redis ./data/shared/
-rsync -rtazP sergeyk@flapjack1.icsi.berkeley.edu:/u/sergeyk/work/vislab-git/data/results ./data/
-rsync -rtazP sergeyk@flapjack1.icsi.berkeley.edu:/u/sergeyk/work/vislab-git/data/mongodump ./data/
+rsync -aP $REMOTE:work/vislab-git/data/shared/rqworkers ./data/shared/
+rsync -aP $REMOTE:work/vislab-git/data/shared/predict ./data_shared/
+rsync -aP $REMOTE:work/vislab-git/data/shared/redis ./data/shared/
+rsync -aP $REMOTE:work/vislab-git/data/shared/db ./data/shared/
+
+rsync -aP $REMOTE:work/vislab-git/data/mongodump ./data/
+rsync -aP $REMOTE:work/vislab-git/data/results ./data/
--- scripts/push_to_cluster.sh
+++ scripts/push_to_cluster.sh
@@ -1,14 +1,12 @@
-#!/bin/zsh
+#!/bin/bash
+echo "usage: push_to_cluster <remote>"
+echo "    rsyncs the repo to remote"
+REMOTE=$1
 
-# rsync the repo to cluster
-# code
-rsync -ravzP --delete --prune-empty-dirs \
-    --exclude "vislab/config.json" --exclude=".DS_Store" --exclude='*.pyc' --exclude='data' --exclude='vislab/tests/_temp' \
-    . sergeyk@flapjack1.icsi.berkeley.edu:/u/sergeyk/work/vislab-git
-rsync -ravzP --delete --prune-empty-dirs \
-    --exclude "vislab/config.json" --exclude=".DS_Store" --exclude='*.pyc' --exclude='data' --exclude='vislab/tests/_temp' \
-    . sergeyk-pc1.vicsi:work/vislab-git
+# Code
+rsync -aP --del --prune-empty-dirs --exclude='*.pyc' \
+    --exclude "vislab/config.json" --exclude='data' --exclude='vislab/tests/_temp' \
+    . $REMOTE:work/vislab-git
 
-# shared data
-rsync -ravzP --delete data/shared/ sergeyk@flapjack1.icsi.berkeley.edu:/u/sergeyk/work/vislab-git/data/shared
-rsync -ravzP --delete data/shared/ sergeyk-pc1.vicsi:work/vislab-git/data/shared
+# Shared data
+rsync -aP --del --prune-empty-dirs data/shared/ $REMOTE:work/vislab-git/data/shared
