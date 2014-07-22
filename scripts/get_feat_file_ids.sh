7f97fa5826450b63a7308f584b2f14e1d3294150
Sergey Karayev <sergeykarayev@gmail.com>
2014-07-22 00:21:05 -0700

    updated results

--- scripts/get_feat_file_ids.sh
+++ scripts/get_feat_file_ids.sh
@@ -1,2 +1,2 @@
-FEAT_DIR=$HOME/work/aphrodite/data/feats
-find $FEAT_DIR -name "*.txt.gz" -exec sh -c "echo {} && zcat {} | cut -d ' ' -f 2 | cut -c 3- > {}.ids.txt" \;
+FEAT_DIR=$HOME/work/vislab-git/data/feats
+find $FEAT_DIR -name "*.txt.gz" -exec sh -c "echo {} && gzcat {} | cut -d ' ' -f 2 | cut -c 3- > {}.ids.txt" \;
